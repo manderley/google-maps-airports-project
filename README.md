8a32f3207dd3b7948fc7cfed78a702ee8dea71d6
Rebecca Edwards <rebecca.edwards@gmail.com>
2016-01-05 15:59:35 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# google-maps-airports-project
---
+++ js/scripts.js
@@ -0,0 +1,73 @@
+var map;
+
+var defaultLat = 40.6413111;
+var defaultLng = -73.77813909;
+
+function loadMap() {
+	
+	var mapOptions = {
+		zoom: 11,
+		center: new google.maps.LatLng(defaultLat, defaultLng),
+		styles: appleStyle
+	};
+
+	// Get the id of the map container div
+	var mapid = document.getElementById('map-container');
+
+	// Create the map
+	map = new google.maps.Map(mapid, mapOptions);
+
+	// Marker creation
+	var newMarker = this.addMarker();
+
+	addInfoWindow(newMarker);
+
+}
+
+function addMarker() {
+	var marker = new google.maps.Marker({
+		position: new google.maps.LatLng(defaultLat, defaultLng),
+		map: map,
+		icon: {
+			url: 'img/airplane-green.png',
+			size: new google.maps.Size(32,32),
+			origin: new google.maps.Point(0,0),
+			anchor: new google.maps.Point(16,32),
+			scaledSize: new google.maps.Size(32,32)
+		},
+		title: 'NEW YORK NY (JFK)'
+	});
+	return marker;
+}
+
+function addInfoWindow(marker) {
+	var contentString = '<div class="infowindowcontent>'+
+		'<div class="row">' +
+		'<p class="total greenbk">78.3%</p>' +
+		'<p class="location">NEW YORK NY</p>' +
+		'<p class="code">JFK</p>' +
+		'</div>' +
+		'<div class="data">' +
+		'<p class="tagbelow">Avg On-Time</p>' +
+		'<p class="label">Arrivals</p>' +
+		'<p class="details">76% (8,590)</p>' +
+		'<p class="label">Departures</p>' +
+		'<p class="details">80.5% (8,589)</p>' +
+		'<p class="coords">' + defaultLat + ', ' + defaultLng + '</p>' +
+		'</div>' +
+		'</div>';
+
+	var infoWindow = new google.maps.InfoWindow({
+		content: contentString,
+		disableAutoPan: false,
+		maxWidth: 300,
+		zIndex: 1
+	});
+
+	google.maps.event.addListener(marker, 'click', function(e) {
+		infoWindow.open(map, marker);
+	})
+}
+
+// Load the map
+google.maps.event.addDomListener(window, 'load', loadMap());
